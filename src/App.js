52dda69bb3dcb47d63c74563d05972ddd36dff79
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-06-07 11:41:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Banner from "./components/Banner";
+import "./App.css";
+
 function App() {
   return (
     <div>
-      <header>
-        Hello World!
-      </header>
+      <Banner />
+      <div
+        className="bg-blue-700 h-screen"
+        style={{
+          backgroundImage: `url(https://lh3.googleusercontent.com/q81BJXmKV2Wr9UpGjMMsT7L1swqoJ-ozGm4lxatjZuKKfWL682ZZq-EYhgaoeiIXyr2l8hEWXOfTT2q7aM-20yHd5zN_bS6hkk1wO1sui_KQEbVu_ksdToYpcYvTDp9s_br3kN3B=w2400)`,
+          backgroundSize: "cover",
+          backgroundRepeat: "repeat-y",
+          backgroundBlendMode: "overlay",
+        }}
+      />
     </div>
   );
 }
