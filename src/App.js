cd7c7bca109318ddc5b1bc254f091b64605f1f1f
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-07-05 09:30:43 +0530

    remove redundancies

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ function App() {
   return (
     <div>
       <Banner />
-      <div
-        className="bg-blue-700 h-screen"
-        style={{
-          backgroundImage: `url(https://lh3.googleusercontent.com/q81BJXmKV2Wr9UpGjMMsT7L1swqoJ-ozGm4lxatjZuKKfWL682ZZq-EYhgaoeiIXyr2l8hEWXOfTT2q7aM-20yHd5zN_bS6hkk1wO1sui_KQEbVu_ksdToYpcYvTDp9s_br3kN3B=w2400)`,
-          backgroundSize: "cover",
-          backgroundRepeat: "repeat-y",
-          backgroundBlendMode: "overlay",
-        }}
-      />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,12 @@ import SearchBar from "./SearchBar";
 const Banner = () => {
   return (
     <div
-      className="bg-blue-700 h-screen"
+      className="bg-blue-700"
       style={{
         backgroundImage: `url(https://lh3.googleusercontent.com/q81BJXmKV2Wr9UpGjMMsT7L1swqoJ-ozGm4lxatjZuKKfWL682ZZq-EYhgaoeiIXyr2l8hEWXOfTT2q7aM-20yHd5zN_bS6hkk1wO1sui_KQEbVu_ksdToYpcYvTDp9s_br3kN3B=w2400)`,
         backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
         backgroundBlendMode: "overlay",
-        backgroundAttachment: "fixed",
+        minHeight: "100vh",
       }}
     >
       <h1 className="py-8 text-6xl text-white text-center font-pacifico">
